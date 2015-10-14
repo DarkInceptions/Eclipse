93183ab8e5e246e639681110d2791511a9e3c03c
Spencer <spencermagill@gmail.com>
2015-10-14 11:44:41 -0500

    Added functionality to convert between Fahrenheit and Celsius

--- Temp_Converter/src/converter/util/C_to_F.java
+++ Temp_Converter/src/converter/util/C_to_F.java
@@ -1,5 +1,17 @@
 package converter.util;
 
-public class C_to_F extends Conversion {
-
+public class C_to_F {
+	public double cIn;
+	public double fOut;
+	
+	public C_to_F() {
+		cIn = 0;
+		fOut = 0;
+	}
+	
+	public double ctof(double x){
+		cIn = x;
+		fOut = ((cIn * 9D/5D) + 32);
+		return fOut;
+	}
 }
--- Temp_Converter/src/converter/util/Conversion.java
+++ Temp_Converter/src/converter/util/Conversion.java
@@ -1,5 +1,13 @@
 package converter.util;
 
 public class Conversion {
-
+	
+	/*public static void main(String[] args) {
+		F_to_C f1 = new F_to_C();
+		C_to_F c1 = new C_to_F();
+		double a = f1.ftoC(212);
+		double b = c1.ctof(100);
+		System.out.println(a);
+		System.out.println(b);
+	}*/
 }
