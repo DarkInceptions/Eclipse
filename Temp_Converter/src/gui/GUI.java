dc736a3725f806efd7e448672bea224a0c6c39b7
Spencer <spencermagill@gmail.com>
2015-10-30 12:36:58 -0500

    Added all functionality

--- Temp_Converter/src/gui/GUI.java
+++ Temp_Converter/src/gui/GUI.java
@@ -1,19 +1,31 @@
 package gui;
 
 import java.awt.*;
+import java.awt.event.*;
 
 import javax.swing.*;
 
 public class GUI {
+	double fIn;
+	double fOut;
+	double cIn;
+	double cOut;
+	
 	JFrame frame;
+	
 	JPanel conversionPanel;
 	JPanel buttonPanel;
+	
 	JButton convert;
+	
 	JRadioButton fc;
 	JRadioButton cf;
+	
 	ButtonGroup group;
+	
 	JLabel label;
 	JLabel ans;
+	
 	JTextField textfield;
 
 	public GUI() {
@@ -37,7 +49,7 @@ public class GUI {
 	}
 
 	public void createFrame() {
-		frame.setSize(new Dimension(300, 200));
+		frame.setSize(new Dimension(400, 200));
 		frame.setLocationRelativeTo(null);
 		frame.setResizable(false);
 		frame.setTitle("Temperature Converter");
@@ -50,15 +62,24 @@ public class GUI {
 
 	public void createConversionPanel() {
 		conversionPanel.setLayout(new GridLayout(0,1));
-
 		textfield.setPreferredSize(new Dimension(100, 30));
-
-		if(fc.isSelected()) {
-			label.setText("Enter \u00B0Fahrenheit:");
-		}
-		if(cf.isSelected()) {
-			label.setText("Enter \u00B0Celsius:");
-		}
+		
+		convert.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(fc.isSelected()) {
+					fIn = Double.parseDouble(textfield.getText());
+					cOut = ((fIn-32)*(5D / 9D));
+					cOut = Math.round(cOut*100.0)/100.0;
+					ans.setText(fIn +" \u00B0Fahrenheit is Equal to " + cOut + " \u00B0Celsius");
+				}
+				if(cf.isSelected()) {
+					cIn = Double.parseDouble(textfield.getText());
+					fOut = ((cIn * 9D/5D) + 32);
+					fOut = Math.round(fOut*100.0)/100.0;
+					ans.setText(cIn +" \u00B0Celsius is Equal to " + fOut + " \u00B0Fahrenheit");
+				}
+			}
+		});
 
 		conversionPanel.add(label);
 		conversionPanel.add(textfield);
@@ -72,5 +93,21 @@ public class GUI {
 		buttonPanel.setLayout(new GridLayout(0,1));
 		buttonPanel.add(fc);
 		buttonPanel.add(cf);
+		
+		fc.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				label.setText("Enter \u00B0Fahrenheit:");
+				textfield.setText(null);
+				ans.setText(null);
+			}
+		});
+		
+		cf.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				label.setText("Enter \u00B0Celsius:");
+				textfield.setText(null);
+				ans.setText(null);
+			}
+		});
 	}
 }
